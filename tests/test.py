c3c3a913cc1d2ef26be10bf0cb058cf1151d9902
sachin <sachinbhusal.dev@gmail.com>
2025-04-25 13:53:15 +0545

    refactor: blank description

--- tests/test.py
+++ tests/test.py
@@ -7,9 +7,9 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.count.graph import workflow as testing_workflow
 from src.inspector.graph import graph as inspector_workflow
 
-user_description = "This is a react graph. It has one agent and one tool node. The agent is an assistant that can perform arithmetic operations."
+user_description = ""
 
-user_valid_input = {"messages": [HumanMessage(content="Add 3 and 4")]}
+user_valid_input = {"messages": [HumanMessage(content="")]}
 
 graph_before_compile = testing_workflow
 
